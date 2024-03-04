e14bbf3ed4c4b033a028935baa82472fbcf26d7b
AlessioRomano95 <romanalex400@gmail.com>
2024-03-05 00:02:30 +0100

    [ADD] descrizione ita

---
+++ asset/script.js
@@ -0,0 +1,10 @@
+// PARI E DISPARI
+
+// 1 recupero gli elementi dal DOM
+// 2 creo la funzione per il numero del PC
+// 3 creo la funzione per recuperare il valore numerico fornito dall'utente
+// 4 recupero gli argomenti da dare nei parametri della funziona 5
+// 5 creo la funzione per verificare se la somma dei miei numeri è pari o dispari
+// 6 apro la lista degli eventi e recupero tutto ciò che ho dichiarato fuori
+// 7 verifico cosa ha scelto l'utente tra pari e dispari e contollo se corrisponde al risultato della somma
+// 8 stampo sul DOM
